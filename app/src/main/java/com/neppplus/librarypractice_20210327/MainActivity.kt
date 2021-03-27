d6f536a581e3adf75fc3fb6ff7527a5723d3ec8f
장혜진 <aslk3366zz@naver.com>
2021-03-27 15:43:22 +0900

    프로필 사진 확대

--- app/src/main/java/com/neppplus/librarypractice_20210327/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20210327/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.neppplus.librarypractice_20210327
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profilePictureImg.setOnClickListener {
+
+//            사진 크게 보는 화면으로 이동
+
+            val myIntent = Intent(this, ViewProfilePictureActivity::class.java)
+            startActivity(myIntent)
+
+
+        }
+
     }
 }
